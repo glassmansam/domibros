ef93ffad6fa66a8999ebf4c1b332ca503985826d
LAPTOP-IPBN20TN\pmlew <pmlewandowski@gmail.com>
2021-10-09 03:55:25 +0200

    piece of shit doesnt wait for button press

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.*;
 import java.util.Properties;
 
@@ -21,7 +23,7 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        logIn(connection);
+        logOption(connection);
         String query = "SELECT id,username, password FROM customer LIMIT 10";
         ResultSet rs = null;
 
@@ -38,7 +40,47 @@ public class Main {
             e.printStackTrace();
         }
     }
-
+public static void logOption(Connection connection){
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Log in or register now!");
+        panel.add(label);
+        String[] options = {"Log in!","Register!"};
+    int option = JOptionPane.showOptionDialog(null, panel, "Enter your login credentials", JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[1]);
+    if(option==0){logIn(connection);}
+    if(option==1){register(connection);}
+}
+public static void register(Connection connection){
+    JPanel panel = new JPanel();
+    JLabel label = new JLabel("Put in your data please!");
+    panel.add(label);
+    JTextField first_nameR = new JTextField(30);
+    JTextField last_nameR = new JTextField(30);
+    JTextField usernameR = new JTextField(30);
+    JPasswordField passwordR = new JPasswordField(30);
+    JTextField phone_numberR = new JTextField(14);
+    JButton button = new JButton("Click me!");
+    panel.add(first_nameR);
+    panel.add(last_nameR);
+    panel.add(usernameR);
+    panel.add(passwordR);
+    panel.add(phone_numberR);
+    button.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e) {
+            String first_name = first_nameR.getText();
+            String last_name = last_nameR.getText();
+            String username = usernameR.getText();
+            String password = passwordR.getPassword().toString();
+            int phone_number=Integer.parseInt(phone_numberR.getText());
+            String query = "INSERT INTO customer (username, password,first_name,last_name,phone_number) VALUES ('" + username + "','" + password + "','" + first_name + "','" + last_name + "','" + phone_number + "')";
+            try (Statement stmt = connection.createStatement()) {
+                stmt.execute(query);
+            } catch (Exception a) {
+                a.printStackTrace();
+            }
+        }
+    });
+    panel.add(button);
+    }
     public static void logIn(Connection connection) {
         username = "";
         password = "";
@@ -55,11 +97,10 @@ public class Main {
             username = userName.getText();
             password = new String(passWord.getPassword());
         }
-        String query = "ISERT INTO customer (username, password) VALUES (Cloud956,passwordisGood)";
-
+        String query = "INSERT INTO customer (username, password) VALUES ('"+username+"','"+password+"')";
 
         try (Statement stmt = connection.createStatement()) {
-            stmt.executeQuery(query);
+             stmt.execute(query);
             }
         catch (Exception e) {
             e.printStackTrace();
