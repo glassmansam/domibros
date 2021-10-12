49301175f4ddbcd3a11112d7c229dde40bff997b
LAPTOP-6B40E7JD\samgl <samglassman@protonmail.com>
2021-10-12 12:00:14 +0200

    timers

--- src/main/java/gui/OrderScreen.java
+++ src/main/java/gui/OrderScreen.java
@@ -1,5 +1,6 @@
 package gui;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -10,10 +11,19 @@ import javafx.scene.layout.VBox;
 import logic.Order;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class OrderScreen extends AnchorPane {
 
+    private final Timer cancelTimer = new Timer();
+    private final Timer estDeliverTimer = new Timer();
+
     private final Order order;
+
+    private boolean canCancel = true;
+
+    private long estimatedTime = System.currentTimeMillis() + 15 * 60 * 10000;
 
     public OrderScreen(Order order) throws IOException {
         this.order = order;
@@ -22,6 +32,27 @@ public class OrderScreen extends AnchorPane {
 
     @FXML
     void initialize() {
+
+        cancelTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                canCancel = false;
+                Platform.runLater(() -> orderStatus.setText("OUT FOR DELIVERY"));
+            }
+        }, 5 * 60 * 10000); //5 min
+
+        estDeliverTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> {
+                    int minutes = (int) (estimatedTime / (60 * 1000));
+                    int seconds = (int) ((estimatedTime / 1000) % 60);
+                    String str = String.format("%d:%02d", minutes, seconds);
+                    estimatedDeliveryTime.setText(str);
+                });
+            }
+        }, 0, 1000);
+
         firstNameLabel.setText(order.getCustomer().getFirstName());
         lastNameLabel.setText(order.getCustomer().getLastName());
         streetLabel.setText(order.getAddress().getStreet());
@@ -29,7 +60,7 @@ public class OrderScreen extends AnchorPane {
         cityLabel.setText(order.getAddress().getCity());
         phoneNumberLabel.setText(order.getCustomer().getPhoneNumber());
 
-        orderStatus.setText(order.getStatus().toString());
+        orderStatus.setText("IN PROCESS");
         numOrderUntilCoupon.setText((order.getCustomer().getAmountOrdered() % 10) + "");
 
         for (CartEntry cartEntry : order.getCart()) {
@@ -45,7 +76,14 @@ public class OrderScreen extends AnchorPane {
 
     @FXML
     void cancelOrder(ActionEvent event) {
-        //check if time elapsed < 5 min
+        if (canCancel) {
+            //execute cancel code
+            orderStatus.setText("CANCELLED");
+            deliveryDriverName.setText("N/A");
+            estimatedDeliveryTime.setText("N/A");
+        } else {
+            System.out.println("Can't cancel :///");
+        }
     }
 
     @FXML
